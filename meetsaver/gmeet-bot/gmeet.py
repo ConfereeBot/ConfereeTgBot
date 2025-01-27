7e8220c77ef19a501a3f42cdc7f0545df4639cfb
Shon Qwezon <shonzbso@gmail.com>
2025-01-27 20:16:04 +0300

    refactor

--- meetsaver/gmeet-bot/gmeet.py
+++ meetsaver/gmeet-bot/gmeet.py
@@ -1,14 +1,21 @@
 import asyncio
-import json
-import pickle
-import subprocess
-from os import getenv, path
+from asyncio import subprocess
+from os import getenv
 
 import nodriver as uc
 
 from .. import utils
 
 logger = utils.logger.setup_logger(__name__)
+
+
+FILENAME = "output.mp4"
+CMD_FFMPEG = f"ffmpeg -y -loglevel warning -framerate 30 \
+    -f x11grab -i :0 -f pulse -i virtual_sink.monitor -ac 2 -b:a 192k {FILENAME}"
+CMD_PULSE = "pulseaudio -D --system=false --exit-idle-time=-1 --disallow-exit --log-level=debug \
+    && pactl load-module module-null-sink sink_name=virtual_sink \
+    && pactl set-default-sink virtual_sink"
+TIMEOUT = 5
 
 
 async def setup_driver() -> uc.Browser:
@@ -34,14 +41,15 @@ async def google_sign_in(driver: uc.Browser):
     logger.info("Completed signing in google account.")
 
 
-async def record_meet(meet_link: str):
+async def record_meet(meet_link: str) -> None | str:
     logger.info(f"Recoring for link: {meet_link}")
+    try:
+        stdout, stderr = await asyncio.wait_for(run_cmd(CMD_PULSE), timeout=TIMEOUT)
+        logger.debug(stderr)
+    except asyncio.TimeoutError:
+        logger.error("Can't run cmd. Exiting it...")
+        return None
 
-    await run_cmd(
-        "pulseaudio -D --system=false --exit-idle-time=-1 --disallow-exit --log-level=debug \
-        && pactl load-module module-null-sink sink_name=virtual_sink \
-        && pactl set-default-sink virtual_sink"
-    )
     driver = await setup_driver()
     await google_sign_in(driver)
     page = await driver.get(meet_link)
@@ -51,10 +59,8 @@ async def record_meet(meet_link: str):
     await driver.wait(5)
 
     logger.info("Start recording...")
-    await run_cmd(
-        "ffmpeg -y -loglevel warning -framerate 30 \
-        -f x11grab -i :0 -f pulse -i virtual_sink.monitor -t 10 -ac 2 -b:a 192k output.mp4"
-    )
+    ffmpeg = await run_cmd(CMD_FFMPEG, True)
+    await asyncio.sleep(10)
     logger.info("Stoped recording.")
 
     exit_btn = await page.find_element_by_text("leave call")
@@ -62,13 +68,25 @@ async def record_meet(meet_link: str):
     await driver.wait(2)
     driver.stop()
 
+    try:
+        ffmpeg.stdin.write(b"q")
+        logger.debug("Terminated. Waiting...")
+        await ffmpeg.stdin.drain()
+        stdout, stderr = await asyncio.wait_for(ffmpeg.communicate(), timeout=TIMEOUT)
+        logger.debug(stderr)
+        return FILENAME
+    except asyncio.TimeoutError:
+        logger.error("Can't terminate ffmpeg. Killing it...")
+        ffmpeg.kill()
 
-async def run_cmd(command):
+
+async def run_cmd(command, on_background=False):
     process = await asyncio.create_subprocess_shell(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=asyncio.subprocess.PIPE
     )
+    if on_background:
+        logger.info(f"Cmd started with PID: {process.pid}")
+        return process
     # Wait for the process to complete
     stdout, stderr = await process.communicate()
-    logger.debug(stderr)
-
     return stdout, stderr
