4a0ef69aee8eb8122c59d63f44f33d85a2f40bcc
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2025-03-05 21:40:00 +0300

    Add buttons navigation, update pyproject.toml aiogram to 3.18 and authors

--- app/roles/user.py
+++ app/roles/user.py
@@ -2,7 +2,7 @@ import os
 
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
-from aiogram.types import FSInputFile, Message
+from aiogram.types import FSInputFile, Message, CallbackQuery
 
 from app.config import messages as msg
 from app.config.roles import Role
@@ -12,9 +12,8 @@ from app.utils import setup_logger
 from app.keyboards import (
     main as main_keyboard,
     choose_recordings_search_method_keyboard as recordings_keyboard,
-    inline_tag_list_for_edit,
-    inline_tag_list_for_recording,
     inline_admin_list,
+    inline_tag_list, manage_tag_inline_keyboard,
 )
 
 logger = setup_logger(__name__)
@@ -43,26 +42,111 @@ async def get_recording(message: Message):
 
 
 @user.message(F.text == "🗂️ Управление тегами")
-async def get_recording(message: Message):
+async def manage_tags(message: Message):
     logger.info("manage_tags_call")
     await message.answer(
         text="Выберите тег или создайте новый",
-        reply_markup=await inline_tag_list_for_edit()
+        reply_markup=await inline_tag_list(
+            on_item_clicked_callback="on_tag_clicked_in_tags_management_mode_callback",
+            on_item_add_clicked_callback="on_tag_add_clicked_in_tags_management_mode_callback",
+            on_cancel_clicked_callback="on_cancel_primary_callback"
+        )
     )
 
 
 @user.message(F.text == "🎥 Записать")
-async def get_recording(message: Message):
+async def record_meeting(message: Message):
     logger.info("manage_tags_call")
     await message.answer(
         text="Выберите тег или создайте новый",
-        reply_markup=await inline_tag_list_for_recording()
+        reply_markup=await inline_tag_list(
+            on_item_clicked_callback="on_tag_clicked_in_recording_mode_callback",
+            on_item_add_clicked_callback="on_tag_add_clicked_in_recording_mode_callback",
+            on_cancel_clicked_callback="on_cancel_primary_callback"
+        )
     )
 
+
 @user.message(F.text == "👨🏻‍💻 Управление админами")
-async def get_recording(message: Message):
+async def manage_admins(message: Message):
     logger.info("manage_admins_call")
     await message.answer(
         text="Выберите админа или создайте новый",
         reply_markup=await inline_admin_list()
+    )
+
+
+@user.callback_query(F.data == "get_recording_by_tag")
+async def get_recording_by_tag(callback: CallbackQuery):
+    await callback.answer("")
+    await callback.message.edit_text(
+        text="Выберите нужный тег",
+        reply_markup=await inline_tag_list(
+            on_item_clicked_callback="on_tag_clicked_in_search_mode",
+            on_item_add_clicked_callback="on_tag_clicked_in_search_mode",
+            on_cancel_clicked_callback="on_cancel_primary_callback"
+        )
+    )
+
+
+@user.callback_query(F.data == "on_tag_clicked_in_recording_mode_callback")
+async def on_tag_clicked_in_recording_mode_callback(callback: CallbackQuery):
+    await callback.answer("")
+    # todo
+
+
+@user.callback_query(F.data == "on_tag_clicked_in_tags_management_mode_callback")
+async def on_tag_clicked_in_tags_management_mode_callback(callback: CallbackQuery):
+    await callback.answer("")
+    await callback.message.edit_text(
+        text="Выберите, что вы хотите сделать с тегом",
+        reply_markup=manage_tag_inline_keyboard
+    )
+
+
+@user.callback_query(F.data == "on_tag_edit_callback")
+async def on_tag_edit_callback(callback: CallbackQuery):
+    await callback.answer("")
+    # todo implement edit logic
+
+
+@user.callback_query(F.data == "on_tag_delete_callback")
+async def on_tag_delete_callback(callback: CallbackQuery):
+    await callback.answer("")
+    # todo implement edit logic
+
+
+@user.callback_query(F.data == "on_cancel_tag_action_callback")
+async def on_cancel_tag_action_callback(callback: CallbackQuery):
+    await callback.answer("")
+    await callback.message.edit_text(
+        text="Выберите нужный тег",
+        reply_markup=await inline_tag_list(
+            on_item_clicked_callback="on_tag_clicked_in_tags_management_mode_callback",
+            on_item_add_clicked_callback="manage_tags_call",
+            on_cancel_clicked_callback="on_cancel_primary_callback"
+        )
+    )
+
+
+@user.callback_query(
+    F.data == "on_cancel_primary_callback"
+)
+async def on_cancel_primary_callback(callback: CallbackQuery):
+    await callback.answer("")
+    await callback.message.edit_text(
+        text="Действие отменено. Выберите новое действие с помощью кнопок под клавиатурой",
+    )
+
+
+@user.callback_query(F.data == "on_cancel_tag_select_for_search_callback")
+async def on_cancel_tag_select_for_recording_callback(callback: CallbackQuery):
+    await callback.answer("")
+    await callback.message.edit_text(
+        text="Выберите тег или создайте новый",
+        reply_markup=await inline_tag_list(
+            on_item_clicked_callback="on_tag_clicked_in_tags_management_mode_callback",
+            on_item_add_clicked_callback="on_tag_add_clicked_in_tags_management_mode_callback",
+            on_cancel_clicked_callback="on_cancel_primary_callback"
+        )
     )
