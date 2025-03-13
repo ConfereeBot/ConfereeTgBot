90b8f971f7eca7ee5b0370002da8a5621ee690c9
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2025-03-13 09:41:37 +0300

    Add meeting db operations, update recording files to use meeting entity

--- app/database/recording_db_operations.py
+++ app/database/recording_db_operations.py
@@ -1,29 +1,27 @@
-from typing import List, Optional
-
-from bson import ObjectId
 from motor.core import AgnosticCollection
 from pymongo.errors import DuplicateKeyError
+from bson import ObjectId
+from typing import Optional
 
 from app.database.database import db
 from app.database.models.recording_DBO import Recording
-from app.database.models.tag_DBO import Tag
 from app.roles.user.user_cmds import logger
 
 
-async def add_recording_to_db(link: str, tag: Tag) -> tuple[bool, str]:
+async def add_recording_to_db(meeting_id: ObjectId, link: str) -> tuple[bool, str, Optional[ObjectId]]:
     """Добавляет новую запись в базу данных."""
-    recording = Recording(link=link, tag=tag)
+    recording = Recording(meeting_id=meeting_id, link=link)
     recordings_collection: AgnosticCollection = db.db["recordings"]
     try:
         await recordings_collection.insert_one(recording.model_dump(by_alias=True))
         logger.info(f"Запись с ссылкой '{link}' успешно добавлена с id: {recording.id}")
-        return True, f"Запись с ссылкой '{link}' успешно добавлена!"
+        return True, f"Запись с ссылкой '{link}' успешно добавлена!", recording.id
     except DuplicateKeyError:
         logger.warning(f"Запись с ссылкой '{link}' уже существует")
-        return False, f"Запись с ссылкой '{link}' уже существует!"
+        return False, f"Запись с ссылкой '{link}' уже существует!", None
     except Exception as e:
         logger.error(f"Ошибка при добавлении записи '{link}': {e}")
-        return False, f"Ошибка: {e}"
+        return False, f"Ошибка: {e}", None
 
 
 async def get_recording_by_id(recording_id: str) -> Optional[Recording]:
@@ -32,8 +30,7 @@ async def get_recording_by_id(recording_id: str) -> Optional[Recording]:
     try:
         recording_doc = await recordings_collection.find_one({"_id": ObjectId(recording_id)})
         if recording_doc:
-            tag = Tag(**recording_doc["tag"])  # Tag обязателен, так что всегда будет
-            return Recording(**recording_doc, tag=tag)
+            return Recording(**recording_doc)
         logger.warning(f"Запись с id '{recording_id}' не найдена")
         return None
     except Exception as e:
@@ -41,32 +38,17 @@ async def get_recording_by_id(recording_id: str) -> Optional[Recording]:
         return None
 
 
-async def get_recordings_by_tag(tag_id: str) -> List[Recording]:
-    """Получает все записи, связанные с определённым тегом."""
-    recordings_collection: AgnosticCollection = db.db["recordings"]
-    recordings = []
-    try:
-        async for recording_doc in recordings_collection.find({"tag._id": ObjectId(tag_id)}):
-            tag = Tag(**recording_doc["tag"])  # Tag обязателен
-            recordings.append(Recording(**recording_doc, tag=tag))
-        return recordings
-    except Exception as e:
-        logger.error(f"Ошибка при получении записей по тегу '{tag_id}': {e}")
-        return []
-
-
-async def get_recording_by_link(link: str) -> Optional[Recording]:
-    """Получает запись по ссылке."""
+async def get_recording_by_meeting_id(meeting_id: str) -> Optional[Recording]:
+    """Получает запись по ID встречи."""
     recordings_collection: AgnosticCollection = db.db["recordings"]
     try:
-        recording_doc = await recordings_collection.find_one({"link": link})
+        recording_doc = await recordings_collection.find_one({"meeting_id": ObjectId(meeting_id)})
         if recording_doc:
-            tag = Tag(**recording_doc["tag"])  # Tag обязателен
-            return Recording(**recording_doc, tag=tag)
-        logger.warning(f"Запись с ссылкой '{link}' не найдена")
+            return Recording(**recording_doc)
+        logger.warning(f"Запись для встречи с id '{meeting_id}' не найдена")
         return None
     except Exception as e:
-        logger.error(f"Ошибка при получении записи с ссылкой '{link}': {e}")
+        logger.error(f"Ошибка при получении записи для встречи с id '{meeting_id}': {e}")
         return None
 
 
